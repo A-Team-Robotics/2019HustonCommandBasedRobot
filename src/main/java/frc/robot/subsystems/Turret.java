37478463c48e4b0b24eb360c7a5231349ff84f5f
A-Team Robotics <6544@a-teamrobotics.com>
2019-02-01 11:24:50 -0500

    Deleted Encoder Class

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -24,23 +24,5 @@ public class Turret extends Subsystem {
     // setDefaultCommand(new MySpecialCommand());
   }
 }
-public class Encoder extends SendableBase{
-  Encoder enc;
-  enc = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-  Encoder sampleEncoder  = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-  sampleEncoder.setMaxPeriod(.1);
-  sampleEncoder.setMinRate(10);
-  sampleEncoder.setDistancePerPulse(5);
-  sampleEncoder.setReverseDirection(true);
-  sampleEncoder.setSamplesToAverage(7);
-  Encoder = sampleEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-  sampleEncoder.reset();
-  Encoder samplEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-  int count = sampleEncoder.get();
-  double distance = sampleEncoder.getRaw();
-  double distance = sampleEncoder.getDistance();
-  double period  = sampleEncoder.getPeriod();
-  double rate = sampleEncoder.getRate;
-  Boolean direction = sampleEncoder.getDirection();
-  Boolean stopped = sampleEncoder.getStopped();
-}
+
+
