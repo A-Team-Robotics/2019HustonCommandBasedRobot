408cd963da01c32e457d0f14fec3c84920ffca8b
Colin-Fox6544 <colin6544aburg@gmail.com>
2019-02-15 21:25:21 -0500

    solenoid

--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
@@ -17,6 +18,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
  * Add your docs here.
  */
 public class Gripper extends Subsystem {
+  private static DoubleSolenoid solOne = new DoubleSolenoid(forwardChannel, reverseChannel);
+  private static DoubleSolenoid solTwo = new DoubleSolenoid(forwardChannel, reverseChannel);
+
+
 
   @Override
   public void initDefaultCommand() {
