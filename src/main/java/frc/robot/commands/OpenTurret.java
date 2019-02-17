5e13c0780910855fb1068f066910839f6f72ec8a
Colin-Fox6544 <colin6544aburg@gmail.com>
2019-02-17 14:21:35 -0500

    lsdktj

--- src/main/java/frc/robot/commands/OpenTurret.java
+++ src/main/java/frc/robot/commands/OpenTurret.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.subsystems.Turret;
 
 public class OpenTurret extends Command {
   public OpenTurret() {
